86eacfe5e100897ea504f74e226eb32cab9f3114
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2024-11-12 02:25:34 +0900

    fix: 接続方法を修正

--- api/index.php
+++ api/index.php
@@ -5,13 +5,17 @@ $port = $_ENV['POSTGRES_PORT'];
 $db = $_ENV['POSTGRES_DATABASE'];
 $user = $_ENV['POSTGRES_USER'];
 $password = $_ENV['POSTGRES_PASSWORD'];
-$endpoint = $_ENV['POSTGRES_ENDPOINT'];
 
-$connection_string = "host=" . $host . " port=" . $port . " dbname=" . $db . " user=" . $user . " password=" . $password . " options='endpoint=" . $endpoint . "' sslmode=require";
+$connection_string = "pgsql:host=$host;port=$port;dbname=$db";
 
-$dbconn = pg_connect($connection_string);
-
-if (!$dbconn) {
-    die("Connection failed: " . pg_last_error());
+try {
+    $db = new PDO($connection_string, $user, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_EMULATE_PREPARES => false,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ]);
+    echo "PostgreSQLに接続成功";
+} catch (PDOException $e) {
+    echo "データベース接続エラー: " . $e->getMessage();
 }
-echo "Connected successfully";
+?>
