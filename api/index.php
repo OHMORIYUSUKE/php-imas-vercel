dca22f9867f3513793045219a65f151751dd2d57
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2024-11-12 02:01:59 +0900

    feat: db接続

--- api/index.php
+++ api/index.php
@@ -1,4 +1,17 @@
 <?php
 
-header('content-type: application/json');
-echo json_encode(['time' => time(), 'date' => date('d.m.Y'), 'tech' => 'Vercel']);
+$host = $_ENV['PG_HOST'];
+$port = $_ENV['PG_PORT'];
+$db = $_ENV['PG_DB'];
+$user = $_ENV['PG_USER'];
+$password = $_ENV['PG_PASSWORD'];
+$endpoint = $_ENV['PG_ENDPOINT'];
+
+$connection_string = "host=" . $host . " port=" . $port . " dbname=" . $db . " user=" . $user . " password=" . $password . " options='endpoint=" . $endpoint . "' sslmode=require";
+
+$dbconn = pg_connect($connection_string);
+
+if (!$dbconn) {
+    die("Connection failed: " . pg_last_error());
+}
+echo "Connected successfully";
